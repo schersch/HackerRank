d8d402455b869d6cae92c9505cd2774227dafe82
Mark Scherschel II <schersch@gmail.com>
2016-08-18 14:34:27 -0700

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,19 @@
+#30 Days of Code Challenge
+#Day 1: Data Types
+
+
+# Declare second integer, double, and String variables.
+ivar = 0
+dvar = 0.0
+svar = ''
+# Read and save an integer, double, and String to your variables.
+ivar = int(input())
+dvar = float(input())
+svar = input()
+# Print the sum of both integer variables on a new line.
+print(i+ivar)
+# Print the sum of the double variables on a new line.
+print(d+dvar)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+svar)
